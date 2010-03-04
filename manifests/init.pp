29bced53ee9fec7e0f01a75587efd6776e1cf896
Brian Henderson <henderb@osuosl.org>
2010-03-03 22:31:01 -0800

    use file for rootkey

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,7 +10,7 @@ class ssh {
     ssh_authorized_key { "rootkey":
         ensure  => present,
         type    => ssh-rsa,
-        key     => "AAAAB3NzaC1yc2EAAAABIwAAAQEAoyMiVDjNOJwiEbmGpJhBh98YdxCkqW0BwGGvBFYQvcaFUw+1/RisVWsJXr/WW2ywwDWT70wXvPLShpNGmbznWhlmJLk+V3XGwTr1Y8afYlNq8KJpHeygE3YulU8opA87VAsmTMjl58M006Nv/3mJ5dvzViZZ8bMfNrhbk8HFrxKdtgn+F4lDYoNW4LmzwzIUrqoxtwECIDh7nBXzWeYzr/7KpYrAEq7QYNu+4IYx3exD0LgCINUkSu/23wuTutsC+SmTgORs8EB00gj7+fud2OCRB16dq+5HK42d7Kt+gIoD3TAyzXA6Ji03R1kysIxKWtGfwblDjMwy5HiqVaze9Q==",
+        key     => file("/etc/puppet/rootkey"),
         name    => root@brianjhenderson.com,
         user    => root,
     }
